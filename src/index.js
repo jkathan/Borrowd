ae50028dac64bcaecc5eadecd75f150868624ad1
Joey Kathan <kathan.joey@gmail.com>
2018-10-05 11:00:41 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import LoanList from './components/loan-list';
+import App from './components/App';
 import store from './store';
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-      <LoanList />
+      <App />
   </Provider>,
   document.getElementById('root')
 );
