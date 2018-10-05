7bac51105776a48b53a8da8cb2a95503a8483598
Joey Kathan <kathan.joey@gmail.com>
2018-10-04 20:48:21 -0400

    initial commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,8 +8,7 @@ const initialState = {
   {type: 'tool', item: 'Wrench', borrower: 'ian', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'},
   {type: 'tool', item: 'Wrench', borrower: 'janet', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'},
   {type: 'tool', item: 'Wrench', borrower: 'greta', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'}
-];
-};
+]};
 
 export const loanReducer = (state=initialState, action) => {
     if (action.type === actions.ADD_LOAN_CARD) {
