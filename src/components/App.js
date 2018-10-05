e95fb3bb09bfd12a80f87d703effa4e83069e376
Joey Kathan <kathan.joey@gmail.com>
2018-10-05 13:14:11 -0400

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-router-dom';
 import LoanList from './loan-list';
 import LoanForm from './loan-form'
@@ -7,7 +7,6 @@ export default function App(props) {
     return (
         <Router>
                 <Switch>
-                    <Redirect exact from="/" to="/items/loans" />
                     <Route exact path='/items/loans' component={LoanList} />
                     <Route exact path='/items/loanform' component={LoanForm} />
                   </Switch>
