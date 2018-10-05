10cf3fb806c1ed3b2d4524e5ab29aff0635dfd30
Joey Kathan <kathan.joey@gmail.com>
2018-10-05 10:40:30 -0400

    initial commit

--- src/components/loan-renew-form.js
+++ src/components/loan-renew-form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export class RenewLoanForm extends React.Component {
+export default class RenewLoanForm extends React.Component {
 	constructor(props) {
         super(props);
         this.state = {
