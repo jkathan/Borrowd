48c6a633da2ec7b04ad8a8ffe95271452f2d03f6
Joey Kathan <kathan.joey@gmail.com>
2018-10-05 11:02:47 -0400

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,6 @@ export default function App(props) {
                     <Route exact path='/items/loans' component={LoanList} />
                     <Route exact path='/items/loanform' component={LoanForm} />
                   </Switch>
-            </div>
         </Router>
     );
 }
