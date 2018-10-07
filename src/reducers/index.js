68fd22303dfffbfcbac5902a04e0d795282d3a0b
Joey Kathan <kathan.joey@gmail.com>
2018-10-06 22:03:15 -0400

    adding searchbar functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,8 @@ import * as actions from '../actions';
 const initialState = {
     loanList: [
   {itemType: 'tool', item: 'Wrench', borrower: 'Bob', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'},
-  {itemType: 'tool', item: 'Wrench', borrower: 'john', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'},
-  {itemType: 'tool', item: 'Wrench', borrower: 'bill', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'},
+  {itemType: 'tool', item: 'Hammer', borrower: 'john', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'},
+  {itemType: 'tool', item: 'Tablesaw', borrower: 'bill', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'},
   {itemType: 'tool', item: 'Wrench', borrower: 'ian', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'},
   {itemType: 'tool', item: 'Wrench', borrower: 'janet', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'},
   {itemType: 'tool', item: 'Wrench', borrower: 'greta', email: 'fake@email.com', phone: '301-555-555', returnDate: '12/8/19'}
