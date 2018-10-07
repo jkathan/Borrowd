89f06e6ab34abd2e84b97de87bb30567194c3002
Joey Kathan <kathan.joey@gmail.com>
2018-10-06 20:44:29 -0400

    adding searchbar functionality

--- src/components/loan-searchable-cards-list.js
+++ src/components/loan-searchable-cards-list.js
@@ -3,8 +3,11 @@ import React from 'react';
 import LoanCard from './loan-card';
 
 export default class LoanSearchList extends React.Component {
+	    constructor(props) {
+        super(props);
+    }
  render() {
-	const loancards = props.loansList.map((loan, index) => (
+	const loancards = this.props.loansList.map((loan, index) => (
             <li key={index}>
                 <LoanCard 
                 listId={index}
