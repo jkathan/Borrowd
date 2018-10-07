c3e0f541be92ea72b25851828fa5271a1fdc815a
Joey Kathan <kathan.joey@gmail.com>
2018-10-07 00:31:16 -0400

    adding searchbar functionality

--- src/components/loan-list.js
+++ src/components/loan-list.js
@@ -25,14 +25,14 @@ export class LoanList extends React.Component {
 
 
     render() {
-        const loanlist = this.props.loansList.map((loan, index) => (
+        /*const loanlist = this.props.loansList.map((loan, index) => (
             <li key={index}>
                 <LoanCard 
                 listId={index}
                 {...loan} />
             </li>
         )
-    )
+    )*/
         let loansList = this.props.loansList.filter(loan =>
             loan.item.toString().toLowerCase().includes(
                 this.state.searchTerm.toString().toLowerCase()
@@ -44,13 +44,7 @@ export class LoanList extends React.Component {
             <div>
                 <SearchBar onChange={searchTerm => this.setState({searchTerm})} />
                 <LoanSearchList loansList = {loansList} />
-                <ul className="list">
-                   <div>
-                        <LoanForm onAdd={(itemType, item, borrower, email, phone, date) => this.addCard(itemType, item, borrower, email, phone, date)}/>
-                        {loanlist}
-                    </div>
-                    
-                </ul>
+
             </div>
         );
     };
@@ -72,7 +66,14 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps)(LoanList);
 
-/*<li>
+/*                <ul className="list">
+                   <div>
+                        <LoanForm onAdd={(itemType, item, borrower, email, phone, date) => this.addCard(itemType, item, borrower, email, phone, date)}/>
+                        {loanlist}
+                    </div>
+                    
+                </ul>
+                <li>
                         <h2><link to = {`/items/loanform`} >Loan out an item</link></h2>
                     </li>
                     */
