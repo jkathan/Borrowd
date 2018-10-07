13f1046d99345896b78e00355572a72c9b54bf2e
Joey Kathan <kathan.joey@gmail.com>
2018-10-06 23:40:45 -0400

    adding searchbar functionality

--- src/components/loan-search-bar.js
+++ src/components/loan-search-bar.js
@@ -4,6 +4,7 @@ export default class SearchBar extends React.Component {
 	    constructor(props) {
         super(props);
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
     /*onSubmit(event) {
         event.preventDefault();
@@ -11,7 +12,7 @@ export default class SearchBar extends React.Component {
     onSubmit={e => e.preventDefault()}*/
     onChange(event) {
     	event.preventDefault();
-    	const searchTerm = (event.target.value);
+    	let searchTerm = event.target.value;
     	this.props.onChange(searchTerm);
     	console.log(searchTerm);
 }
@@ -20,7 +21,7 @@ export default class SearchBar extends React.Component {
     return (
       <form onSubmit={e => e.preventDefault()}>
       		<label>Search by Item</label>
-	        <input type="text" placeholder="Search..." value={this.state.value}
+	        <input type="text" placeholder="Search..." 
 	        onChange={this.props.onChange} />
       </form>
     );
