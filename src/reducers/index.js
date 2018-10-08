16f9ce3de991da8778e438d181e4363e16530471
Joey Kathan <kathan.joey@gmail.com>
2018-10-08 15:55:14 -0400

    adding searchbar functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -22,7 +22,7 @@ export const loanReducer = (state=initialState, action) => {
                   borrower: action.borrower, 
                   email: action.email, 
                   phone: action.phone, 
-                  date: action.date.toString()
+                  returnDate: action.date.toString()
             }]
             });
     }
