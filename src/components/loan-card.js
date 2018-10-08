9cc35a45d9ce93441037bd815ef45440d6e9a2c4
Joey Kathan <kathan.joey@gmail.com>
2018-10-07 23:21:22 -0400

    adding searchbar functionality

--- src/components/loan-card.js
+++ src/components/loan-card.js
@@ -10,7 +10,7 @@ export class LoanCard extends React.Component  {
   returnLoanItem(e, index){
     e.preventDefault();
     console.log(index);
-   //this.props.returnLoanItem(index);
+   returnLoanItem(index);
 }
 //would rather do handleclick in parent but cant specifiy button
 render() {    
