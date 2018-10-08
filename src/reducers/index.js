7f2c978d53e290a41811706dcefa9e90ebc20243
Joey Kathan <kathan.joey@gmail.com>
2018-10-08 12:36:15 -0400

    adding searchbar functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -28,18 +28,19 @@ export const loanReducer = (state=initialState, action) => {
     }
 
    else if (action.type === actions.RETURN_ITEM) {
-      return 
+      return Object.assign({}, state, {
         loanList: state.loanList.filter((id) => id.listId !== action.itemId)
-      }
+      };
+    }
     
 
     else if (action.type === actions.RENEW_ITEM) {
-      return 
+      return Object.assign({}, state, {
         loanList: state.loanList.map((i) => (
         i.listId === action.itemId ? 
         {...i, returnDate: action.returnDate}: i));
     }
-  
+  }
     return state;
 //};
 }
