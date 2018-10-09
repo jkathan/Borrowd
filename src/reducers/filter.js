496dd1cf2c3e95ebca7b1d8620f3b6696ee2cd0d
Joey Kathan <kathan.joey@gmail.com>
2018-10-09 00:58:03 -0400

    adding searchbar functionality

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -2,14 +2,12 @@ const filtersReducerDefaultState = {
 	searchTerm: ''
 }
 export const fildersReducer = ((state = filtersReducerDefaultState, action) => {
-export default  => {
-    switch (action.type) {
-        case 'FILTER_TEXT':
+    if (action.type === FILTER_TEXT) {
             return {
                 ...state,
                 searchTerm: action.text
             };
-        default:
+        }
             return state;
     }
-}
+)
