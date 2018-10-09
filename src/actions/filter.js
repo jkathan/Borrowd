6669f8f71876e701563b31bea86a6d9eb567b839
Joey Kathan <kathan.joey@gmail.com>
2018-10-09 14:58:59 -0400

    adding format functionality

--- src/actions/filter.js
+++ src/actions/filter.js
@@ -3,9 +3,9 @@ export const filterText = (text) => ({
     type: 'FILTER_TEXT',
     text
 });
-
+/*
 export const SORT_ITEMS = 'SORT_ITEMS';
 export const filterDate = (date) => ({
     type: 'SORT_ITEMS',
     sortFilter
-});
+});*/
