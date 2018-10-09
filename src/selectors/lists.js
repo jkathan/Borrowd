302a6533a86cb01b433189510db4e57b8fe350ca
Joey Kathan <kathan.joey@gmail.com>
2018-10-09 16:02:37 -0400

    adding format functionality

--- src/selectors/lists.js
+++ src/selectors/lists.js
@@ -1,14 +1,16 @@
 //import moment from 'moment'
 
 export default function getVisibleItem(loanList, {text, sortFilter}) {
-	console.log(sortFilter.sortFilter.returnDate);
+	console.log(text.searchTerm);
+	//console.log(sortFilter.sortFilter);
     return loanList.loanList.filter(item => {
         const textMatch =
             item.item.toString().toLowerCase().includes(text.searchTerm) 
             return textMatch;
-        }).sort((a, b) => {
+        })
+    /*.sort((a, b) => {
     	if (sortFilter === "Due Date") {
     		return a.sortFilter.returnDate - b.sortFilter.returnDate;
     	}
-    })
+    })*/
 }
