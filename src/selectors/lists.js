f5770555a24768eda2a1597987c4e1a34be4484c
Joey Kathan <kathan.joey@gmail.com>
2018-10-09 10:49:57 -0400

    adding search functionality

--- src/selectors/lists.js
+++ src/selectors/lists.js
@@ -1,7 +1,8 @@
 export default function getVisibleItem(loanList, text) {
+	console.log(Object.keys(loanList));
 	console.log(loanList);
 	console.log(text);
-    return Object.listId(loanList).filter(item => {
+    return Object.keys(loanList).filter(item => {
         const textMatch =
             item.item.toLowerCase().includes(text.toLowerCase()) 
             return textMatch;
