6ff8abf376c44d2dc37188be21524aa2a71e94f0
Joey Kathan <kathan.joey@gmail.com>
2018-10-09 01:04:09 -0400

    adding searchbar functionality

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './components/App';
-import LoanList from './components/loan-list'
+import LoanList from './components/loan-list';
+import LoanList from './components/loan-page';
 import store from './store';
 import './index.css';
 
