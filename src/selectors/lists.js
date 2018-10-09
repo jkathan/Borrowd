7d805cd4c7140e85d597584019003543efb09cdb
Joey Kathan <kathan.joey@gmail.com>
2018-10-09 10:13:59 -0400

    adding searchbar functionality

--- src/selectors/lists.js
+++ src/selectors/lists.js
@@ -1,4 +1,5 @@
 export default function getVisibleItem(loanList, text) {
+	console.log(loanList);
     return loanList.filter(item => {
         const textMatch =
             item.item.toLowerCase().includes(text.toLowerCase()) 
