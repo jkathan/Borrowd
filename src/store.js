c4de330cda49e95cc297afb800ef1f2a80e4fd23
Joey Kathan <kathan.joey@gmail.com>
2018-10-10 01:32:34 -0400

    adding format functionality

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {createStore, combineReducers} from 'redux'
 
 import {loanReducer} from './reducers/index';
 import {filtersReducer} from './reducers/filter';
-import {borrowReducer} from './reducers/filter';
+import {borrowReducer} from './reducers/borrow';
 
 const rootReducer =
         combineReducers({
