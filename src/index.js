7b358bba3f759a6a5c10fca4361e3465ff91945a
Joey Kathan <kathan.joey@gmail.com>
2018-10-10 00:50:26 -0400

    adding format functionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './components/App';
-import LoanList from './components/loan-list';
-import LoanPage from './components/loan-page';
+
 import store from './store';
 import './index.css';
 
