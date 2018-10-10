5865f7ee8eeddb15c385c0a027deffdb41470b82
Joey Kathan <kathan.joey@gmail.com>
2018-10-10 14:21:22 -0400

    adding format functionality

--- src/components/Item-list.js
+++ src/components/Item-list.js
@@ -31,7 +31,7 @@ export class ItemList extends React.Component {
     }
 */
     render() {
-        const checkedOutItems = this.props.loanList
+        const checkedOutItems = this.props.loanList.loanList
         console.log(checkedOutItems);
         /*const itemCheckedOutList = this.props.loanList.map((item, index) => (
                 <ItemCheckoutCard 
