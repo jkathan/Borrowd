b0fa56509c1c175fecd2ce22566b2c67604e8e2d
Joey Kathan <kathan.joey@gmail.com>
2018-10-11 12:27:23 -0400

    adding page components in app.js

---
+++ src/components/side-bar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+//import './sidebar.css';
+
+class Sidebar extends React.Component {
+    render () {
+    return (
+        <div className="sidebar sidebar-left">
+            <nav className="folder-menu">
+                <ul className="folder-menu-list">
+                    <li className="folder-menu-list-item">
+                        <Link to= '/'>
+                            Dashboard
+                        </Link>
+                    </li>                                
+                    <li className="folder-menu-list-item">
+                        <Link to= '/items/loans'>
+                            Loans
+                        </Link>
+                    </li>
+                    <li className="folder-menu-list-item">
+                        <Link to= '/items/loans'>
+                            Borrows
+                        </Link>
+                    </li>
+        
+                </ul>
+            </nav>
+        </div>
+        );
+    }
+}
+
+
+export default connect()(Sidebar);
