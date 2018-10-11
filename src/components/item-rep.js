bf20c73e1e15452a89ec9f4ce68e6059544d9963
Joey Kathan <kathan.joey@gmail.com>
2018-10-11 00:54:10 -0400

    adding page components in app.js

--- src/components/item-rep.js
+++ src/components/item-rep.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {addCard} from '../actions/index';
 import {removeItemFromList} from '../actions/index';
 import ItemLoanForm from './item-loan-form';
+import {connect} from 'react-redux';
 
 class ItemRepo extends React.Component {
 	constructor(props) {
@@ -19,8 +20,9 @@ class ItemRepo extends React.Component {
         this.props.history.push(`/items/itemedit`);
     }*/
     onAdd(borrower, email, phone, date) {
-    	itemType = this.props.itemType;
-    	item = this.props.item;
+    	var itemType = this.props.itemType.value.trim();
+    	var item = this.props.item.value.trim();
+    	const dateAdded = moment().format('YYYY-MM-DD');
     	this.props.dispatch(
             //wahat is boardID and why is it necessary?
         addCard(itemType, item, borrower, email, phone, date, dateAdded, null)
@@ -29,7 +31,7 @@ class ItemRepo extends React.Component {
     returnItem(e, index){
 	    e.preventDefault();
 	    console.log(index);
-	    this.props.dispatch(returnLoanItem(index));
+	    this.props.dispatch(removeItemFromList(index));
 }
 //edit button should be a link
 //new form form for borrow and forms
