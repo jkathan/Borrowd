4caa08b964133d8356f99e87a32b3f9f4d68b757
Joey Kathan <kathan.joey@gmail.com>
2018-10-11 00:15:32 -0400

    adding page components in app.js

--- src/components/Item-list.js
+++ src/components/Item-list.js
@@ -9,7 +9,7 @@ import ItemCheckoutCard from './item-loan-card';
 import ItemBorrowCard from './item-borrow-card'
 import {borrowList} from '../actions/index';
 import {returnBorrowItem} from '.../actions/index';
-import {loanList} from '.../actions/index';
+import {loanList} from '../actions/index';
 import {filterText} from '../actions/filter';
 //import getVisibleListItem from '../selectors/items';
 import {filterDate} from '../actions/filter';
