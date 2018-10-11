6ae9966cda26e5032a5cd7c1302917560e3bd816
Joey Kathan <kathan.joey@gmail.com>
2018-10-11 01:10:32 -0400

    adding page components in app.js

--- src/components/Item-list.js
+++ src/components/Item-list.js
@@ -34,7 +34,7 @@ export class ItemList extends React.Component {
 
     render() {
         
-        
+        console.log(this.props.loanList);
         const itemCheckedOutList = this.props.loanList.map((item, index) => (
                 <ItemCheckoutCard 
                 listId={index}
