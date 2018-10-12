746c5a68877ad22f6fc5743ea92d76e158facca7
Joey Kathan <kathan.joey@gmail.com>
2018-10-12 17:29:55 -0400

    fixing bugs

--- src/selectors/lists.js
+++ src/selectors/lists.js
@@ -2,9 +2,9 @@
 
 export default function getVisibleItem(loanList, text) {
 	//console.log(sortFilter.sortFilter);
-
+console.log(loanList.loanList);
     return loanList.loanList
-
+    
     .filter(item => {
         const textMatch =
             item.item.toString().toLowerCase().includes(text.searchTerm) 
