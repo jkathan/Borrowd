f3e7ff366ece91b5606983d19bf9ccd2d3c41c01
Joey Kathan <kathan.joey@gmail.com>
2018-10-16 16:03:28 -0400

    updating pages

--- src/components/item-rep.js
+++ src/components/item-rep.js
@@ -66,8 +66,8 @@ class ItemRepo extends React.Component {
 //edit button should be a link
 //new form form for borrow and forms
 	render() {
-		console.log(this.props.itemType);
-		console.log(this.props);
+		//console.log(this.props.itemType);
+		//console.log(this.props);
 		return (
 			<li>
 				<div>
