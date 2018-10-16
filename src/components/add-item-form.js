3e5993f8406bdacd5816e910856e621ef4a68949
Joey Kathan <kathan.joey@gmail.com>
2018-10-16 13:22:53 -0400

    updating pages

--- src/components/add-item-form.js
+++ src/components/add-item-form.js
@@ -4,7 +4,7 @@ import moment from 'moment'
 import {addItem} from '../actions/index';
 import './add-form.css';
 import {required, nonEmpty, email} from '../validators';
-import {Field, reduxForm, SubmissionError, focus} from 'redux-form';
+import {Field, reduxForm, SubmissionError, focus, reset} from 'redux-form';
 import Input from './input';
 //how do I 
 export class AddItemForm extends React.Component {
@@ -25,7 +25,7 @@ export class AddItemForm extends React.Component {
         addItem(itemType, item, null)
 
         );
-
+        this.props.dispatch(reset(addItemForm));
     }
 
 render() {
