741aac6633c8885ef4a3a8091486882c6e74affe
Joey Kathan <kathan.joey@gmail.com>
2018-10-16 14:26:06 -0400

    updating pages

--- src/components/borrow-list.js
+++ src/components/borrow-list.js
@@ -78,7 +78,7 @@ const mapStateToProps = state => ({
         state.loanList
     );*/
     //return {
-        //borrowList: getVisibleBorrowItem(state.borrowList, state.filters)
+        borrowList: getVisibleBorrowItem(state.loanList.borrowlist, state.filters)
     //};
 
 })
