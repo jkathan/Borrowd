e8bccc8eef1eeb5c7016469a818364e270c88226
Joey Kathan <kathan.joey@gmail.com>
2018-10-16 12:33:39 -0400

    updating pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './components/App';
 import ItemList from './components/Item-list';
-import AddItemForm from './add-item-form';
+import AddItemForm from './components/add-item-form';
 
 import store from './store';
 import './index.css';
